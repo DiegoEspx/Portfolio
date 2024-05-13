480225cbeed228a06ee7b66526ecea3c70fb2c01
diego <diego.guerreroe@campusucc.edu.co>
2024-05-13 16:56:45 -0500

    responsive de about me

--- src/app/screens/About.tsx
+++ src/app/screens/About.tsx
@@ -5,19 +5,22 @@ const About = () => {
 
   return (
     <main className="flex items-center justify-center w-full h-full">
-      <div className="max-w-screen-lg flex bg-blue-900 p-28 w-full">
-        {/* Contenedor del texto */}
-        <div className="w-1/2 pr-8">
-            <h1 className="text-3xl md:text-5xl mb-5 p-3 text-gradient2">
-              <strong>{t('about')}</strong>
-            </h1>
-          <div className="text-lg md:text-xl text-white text-justify overflow-y-auto" style={{ maxHeight: '300px', direction: 'rtl' }}>
+      <div className="max-w-screen-lg flex flex-col-reverse md:flex-row bg-blue-900 p-8 md:p-28 ml-20">
+        {/*Contenedor de la imagen*/}
+        <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0 md:pl-20">
+          <img src="../../assets/icons/logo.svg" className="w-full h-auto" alt="Logo" />
+        </div>
+          {/*Contenedor del texto*/}
+        <div className="w-full md:w-1/2 md:pr-20">
+            {/*sobre mi*/}
+          <h1 className="text-3xl md:text-5xl mb-5 p-3 text-gradient2">
+            
+            <strong>{t('about')}</strong>
+          </h1>
+          <div className="text-lg md:text-xl text-white text-justify overflow-y-auto xs:ml-12" style={{ maxHeight: '300px', direction: 'rtl' }}>
+            {/*descripcion*/}
             {t('textTwo')}
           </div>
-        </div>
-        {/* Contenedor de la imagen */}
-        <div className="w-1/2 content-center ">
-          <img src="../../assets/icons/logo.svg" className="w-full h-auto" alt="Logo" />
         </div>
       </div>
     </main>
--- src/app/screens/Home.tsx
+++ src/app/screens/Home.tsx
@@ -5,19 +5,16 @@ const Home = () => {
 
   return (
     <main className="flex flex-col lg:flex-row items-center justify-center w-full h-full">
-      <div className="md:px-12 lg:px-4 max-w-screen-lg xl:px-3 flex flex-col lg:flex-row items-center justify-center bg-blue-900 p-8 lg:p-24 mb-8 lg:mb-24"> {/* Tamaño fijo del cuadro azul y padding reducido */}
+      <div className="md:px-12 lg:px-4 max-w-screen-lg xl:px-32 flex flex-col lg:flex-row items-center justify-center bg-gradient-to-r from-blue-800 to-blue-900 p-8 lg:p-16 mb-8 lg:mb-16 rounded-lg shadow-2xl">
         {/* Texto a la izquierda */}
         <div className="w-full md:w-1/2 lg:w-1/2 pr-0 md:pr-8 lg:pr-16 mb-8 lg:mb-0">
-          <h1 className="text-xl md:text-5xl text-app-light-background-2 text-gradient1 p-2">
-            <strong>{t('home')}</strong>
-          </h1>
-    
-          <p className="text-lg md:text-xl text-white mt-6 text-justify ">
-            {t('textOne')}
-          </p>
+          
+          <h1 className="text-2xl md:text-5xl text-gradient1 font-bold mb-4 p-2">{t('home')}</h1>
+          <p className="text-lg md:text-xl text-white leading-relaxed text-justify">{t('textOne')}</p>
         </div>
-        <div className="w-full md:w-1/2 lg:w-1/2"> {/* Oculta en pantallas pequeñas y muestra en pantallas medianas y más grandes */}
-          <img src="../../assets/icons/logo.svg" className="w-full h-auto" alt="Logo" />
+        {/* Imagen a la derecha */}
+        <div className="w-full md:w-1/2 lg:w-1/2">
+          <img src="../../assets/icons/logo.svg" className="w-full h-auto rounded-lg" alt="Logo" />
         </div>
       </div>
     </main>
